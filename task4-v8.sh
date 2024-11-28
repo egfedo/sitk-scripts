96304714b1ce9efb4b3b9d4fc0f4f1979ee22759
Egor Fedorov <34489396+egfedo@users.noreply.github.com>
2024-11-28 16:55:30 +0300

    Add files via upload

--- task4-v8.sh
+++ task4-v8.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # Удаление всех путей
 echo "Default routes"
 ip route flush all
@@ -25,10 +26,10 @@ echo "  ethernets:" >> /etc/netplan/60-config.yaml
 echo "    enp0s8:" >> /etc/netplan/60-config.yaml
 echo "      dhcp4: no" >> /etc/netplan/60-config.yaml
 echo "  vlans:" >> /etc/netplan/60-config.yaml
-echo "    vlan2222:" >> /etc/netplan/60-config.yaml
-echo "      id: 2222" >> /etc/netplan/60-config.yaml
+echo "    vlan22:" >> /etc/netplan/60-config.yaml
+echo "      id: 22" >> /etc/netplan/60-config.yaml
 echo "      link: enp0s8" >> /etc/netplan/60-config.yaml
-echo "      addresses: [12.43.15.19/18]" >> /etc/netplan/60-config.yaml
+echo "      addresses: [112.43.60.254/11]" >> /etc/netplan/60-config.yaml
 
 # Перезагрузка системы
 echo "Rebooting"
@@ -36,4 +37,4 @@ sleep 1
 reboot
 
 
-# UB 1
+# UB 3
