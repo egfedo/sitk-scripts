abd3027e9d711943983492cfc7fce39610a6159e
Egor Fedorov <34489396+egfedo@users.noreply.github.com>
2024-11-28 17:17:45 +0300

    Add files via upload

--- task4-v8.sh
+++ task4-v8.sh
@@ -5,7 +5,6 @@ echo "Default routes"
 ip route flush all
 
 # Очистка правил iptables
-echo "Default iptables rules"
 iptables -F
 iptables -X
 iptables -t nat -F
@@ -25,16 +24,21 @@ echo "  renderer: networkd" >> /etc/netplan/60-config.yaml
 echo "  ethernets:" >> /etc/netplan/60-config.yaml
 echo "    enp0s8:" >> /etc/netplan/60-config.yaml
 echo "      dhcp4: no" >> /etc/netplan/60-config.yaml
+echo "    enp0s9:" >> /etc/netplan/60-config.yaml
+echo "      dhcp4: no" >> /etc/netplan/60-config.yaml
 echo "  vlans:" >> /etc/netplan/60-config.yaml
+echo "    vlan2222:" >> /etc/netplan/60-config.yaml
+echo "      id: 2222" >> /etc/netplan/60-config.yaml
+echo "      link: enp0s8" >> /etc/netplan/60-config.yaml
+echo "      addresses: [12.43.15.20/18]" >> /etc/netplan/60-config.yaml
 echo "    vlan22:" >> /etc/netplan/60-config.yaml
 echo "      id: 22" >> /etc/netplan/60-config.yaml
-echo "      link: enp0s8" >> /etc/netplan/60-config.yaml
-echo "      addresses: [112.43.60.254/11]" >> /etc/netplan/60-config.yaml
+echo "      link: enp0s9" >> /etc/netplan/60-config.yaml
+echo "      addresses: [112.43.60.200/11]" >> /etc/netplan/60-config.yaml
 
 # Перезагрузка системы
 echo "Rebooting"
-sleep 1
 reboot
 
 
-# UB 3
+# UB R
